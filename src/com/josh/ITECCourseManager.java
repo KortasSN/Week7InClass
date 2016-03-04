309c9d6098017be7257553cabdf19b85fceff4d2
ec0662sr <Ramona05!>
2016-03-03 19:37:02 -0600

    asdfas

--- src/com/josh/ITECCourseManager.java
+++ src/com/josh/ITECCourseManager.java
@@ -22,7 +22,7 @@ public class ITECCourseManager {
         datacomCourse.addStudent("Flora");
 
         datacomCourse.writeCourseInfo();
-//asdfasd
+//asdfasdasdfasdf
         ITECCourse javaProgramming =
                 new ITECCourse("Java Programming", 2545, 24);
 
